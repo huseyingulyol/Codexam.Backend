ba364554f01ff865177c7732b86b83acb63cc6d7
huseyingulyol <streamworld15@gmail.com>
2025-02-26 11:54:18 +0300

    make nullable virtual role

--- Codexam.WebAPI/Entities/User.cs
+++ Codexam.WebAPI/Entities/User.cs
@@ -1,11 +1,14 @@
-﻿namespace Codexam.WebAPI.Entities
+﻿using System.Text.Json.Serialization;
+
+namespace Codexam.WebAPI.Entities
 {
     public class User : BaseEntity
     {
         public string Email { get; set; }
         public string PasswordHash { get; set; }
         public int RoleId { get; set; }
-        public virtual Role Role { get; set; }
+        [JsonIgnore]
+        public virtual Role? Role { get; set; }
     }
 
 }
